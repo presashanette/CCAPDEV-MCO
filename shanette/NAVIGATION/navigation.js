39b7e90bf1e238fa3d83ab10793728d87e776021
Hanna-Angela <hanna_delossantos@dlsu.edu.ph>
2024-02-10 21:05:16 +0800

    with nav

---
+++ shanette/NAVIGATION/navigation.js
@@ -0,0 +1,35 @@
+
+/**
+ * isLoggedIn = false;
+ * ^ turns true when user logs in
+ * 
+ * if isLoggedIn == true
+ *      change:
+ *      Log In -> Log Out
+ *      link   -> link
+ */
+
+function filter() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+            
+        }
+    }
+
+    
+}  
